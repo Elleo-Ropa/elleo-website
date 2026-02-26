ba42a1a3aebd35a83f804f0e50f580987cf5cd72
Elleo Group <elleogroup@Ropa-iMac.local>
2026-02-27 10:30:25 +1100

    chore: remove unused Marquee and api/photos.js files

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,7 +45,7 @@ const Hero: React.FC = () => {
   }, [images]);
 
   return (
-    <section className="relative w-full min-h-[100dvh] bg-black overflow-hidden flex flex-col justify-end pb-6 md:pb-10">
+    <section className="relative w-full min-h-[100vh] bg-black overflow-hidden flex flex-col justify-end pb-6 md:pb-10">
       {/* 
         The Background (Crossfading Slideshow) 
       */}
